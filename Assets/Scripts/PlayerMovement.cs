8034a72b1ea6e6bc3bde23356223e1904f0d33bb
Shreyansh <shreyanshgupta118@gmail.com>
2026-09-17 00:02:24 +0530

    player movement feels like human movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,35 +1,68 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
-public class PLayerMovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
     Vector2 movementInput;
     Animator animator;
 
-    void Start(){
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float acceleration = 20f;
+    [SerializeField] float deceleration = 25f;
+
+    [Header("Jump")]
+    [SerializeField] float jumpForce = 5f;
+
+    void Start()
+    {
         rb = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
-        Run();
+        animator.SetBool("isRunning", movementInput != Vector2.zero);
     }
 
-    void Run()
+    void FixedUpdate()
     {
-        rb.AddForce(new Vector3(movementInput.x * 2f, 0, movementInput.y * 2f), ForceMode.Force);
-        if(movementInput != Vector2.zero)
-        {
-            animator.SetBool("isRunning", true);
-        }
-        else
-        {
-            animator.SetBool("isRunning", false);
-        }
+        Move();
     }
+
+    void Move()
+    {
+        Vector3 targetDirection = new Vector3(
+            movementInput.x,
+            0f,
+            movementInput.y
+        ).normalized;
+
+        Vector3 targetVelocity = targetDirection * moveSpeed;
+
+        Vector3 currentHorizontalVelocity = new Vector3(
+            rb.linearVelocity.x,
+            0f,
+            rb.linearVelocity.z
+        );
+
+        float currentAcceleration =
+            movementInput != Vector2.zero ? acceleration : deceleration;
+
+        Vector3 newVelocity = Vector3.MoveTowards(
+            currentHorizontalVelocity,
+            targetVelocity,
+            currentAcceleration * Time.fixedDeltaTime
+        );
+
+        rb.linearVelocity = new Vector3(
+            newVelocity.x,
+            rb.linearVelocity.y,
+            newVelocity.z
+        );
+    }
+
     void OnMove(InputValue value)
     {
         movementInput = value.Get<Vector2>();
@@ -37,9 +70,9 @@ public class PLayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        if(value.isPressed)
+        if (value.isPressed)
         {
-            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
 }
